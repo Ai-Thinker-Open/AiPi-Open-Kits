22c22684807b718d9fe0de826d883448822b299f
SeaHi-Mo <seahi@foxmail.com>
2023-07-26 14:20:04 +0800

    update

--- AiPi-DSL_Dashboard/uni_hb_m_solution/unione_lite_app_hb_m/build/objects.mk
+++
@@ -1,7 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lbargein -lual-aik -lengine -lual-osal -lfft_fix_mfcc_nrd -lmatrix_op -ldsp -lunihal -lm
