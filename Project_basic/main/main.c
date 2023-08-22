80cb97ea2f8482c56fa5feb5b0ed8e34e14e8ed3
SeaHi-Mo <seahi@foxmail.com>
2023-08-22 16:37:45 +0800

    add Project_basic File

---
+++ Project_basic/main/main.c
@@ -0,0 +1,28 @@
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-08-22
+ *
+ * @copyright Copyright (c) 2023
+ *
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include "board.h"
+#include "log.h"
+
+
+#define DBG_TAG "MAIN"
+
+
+int main(void)
+{
+    board_init();
+    LOG_I("Hello world");
+}
