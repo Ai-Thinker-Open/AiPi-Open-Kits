09e5c586e14ece70ae72f31003d714df97549df8
SeaHi-Mo <seahi@foxmail.com>
2023-11-15 15:34:59 +0800

    “frist”

---
+++ update_sdk.sh
@@ -0,0 +1,4 @@
+mv bl61x_SDK/os/bsp bl61x_SDK/os/bsp_old 
+cp -r bl61x_SDK/AiPi_bsp  bl61x_SDK/os
+mv bl61x_SDK/os/AiPi_bsp bl61x_SDK/os/bsp
+mv bl61x_SDK/dclocl bl61x_SDK/os/components/graphics/lvgl/extra/widgets
