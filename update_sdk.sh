93ae872613662841211bae8b8eb07776d2cf115f
SeaHi-Mo <seahi@foxmail.com>
2023-12-05 15:10:53 +0800

    add function

--- update_sdk.sh
+++ update_sdk.sh
@@ -1,3 +1,5 @@
 mv bl61x_SDK/os/bsp/common bl61x_SDK/os/bsp/common_old
 cp -r bl61x_SDK/AiPi_bsp/common  bl61x_SDK/os/bsp/common
-mv bl61x_SDK/dclock bl61x_SDK/os/components/graphics/lvgl/extra/widgets
+cp -r bl61x_SDK/AiPi_bsp/dclock bl61x_SDK/os/components/graphics/lvgl/extra/widgets
+rm /bl61x_SDK/os/components/utils/CMakeLists.txt
+mv bl61x_SDK/AiPi_bsp/CMakeLists.txt bl61x_SDK/os/components/utils/
